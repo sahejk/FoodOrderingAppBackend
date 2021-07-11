375dd099cd99f668d52b4227619db7830a7178eb
Sahej Kaur Makkar <sahejk@fb.com>
2021-07-11 19:08:40 +0800

    add item controller endpoint

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/ItemService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/ItemService.java
@@ -1,16 +1,18 @@
 package com.upgrad.FoodOrderingApp.service.businness;
 
 import com.upgrad.FoodOrderingApp.service.dao.ItemDao;
+import com.upgrad.FoodOrderingApp.service.dao.OrderDao;
 import com.upgrad.FoodOrderingApp.service.dao.OrderItemDao;
 import com.upgrad.FoodOrderingApp.service.entity.ItemEntity;
 import com.upgrad.FoodOrderingApp.service.entity.OrderItemEntity;
 import com.upgrad.FoodOrderingApp.service.entity.OrdersEntity;
+import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
 import com.upgrad.FoodOrderingApp.service.exception.ItemNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class ItemService {
@@ -19,6 +21,9 @@ public class ItemService {
 
     @Autowired
     private ItemDao itemDao;
+
+    @Autowired
+    private OrderDao orderDao;
 
     public List<OrderItemEntity> getItemsByOrder(OrdersEntity orderEntity) {
         return orderItemDao.getItemsByOrder(orderEntity);
@@ -34,5 +39,39 @@ public class ItemService {
         } else {
             return itemEntity;
         }
+    }
+
+    @Transactional
+    public List<ItemEntity> getItemsByPopularity(RestaurantEntity restaurantEntity) {
+
+        // List to store all items ordered in a restaurant
+        List<ItemEntity> itemEntityList = new ArrayList<>();
+
+        // Gets all the orders placed in the restaurant
+        for (OrdersEntity orderEntity : orderDao.getOrdersByRestaurant(restaurantEntity)) {
+            // Gets items from each order placed in the restaurant
+            for (OrderItemEntity orderItemEntity : orderItemDao.getItemsByOrder(orderEntity)) {
+                itemEntityList.add(orderItemEntity.getItem());
+            }
+        }
+
+        // Load all the item entities to hashmap
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for (ItemEntity itemEntity : itemEntityList) {
+            Integer count = map.get(itemEntity.getUuid());
+            map.put(itemEntity.getUuid(), (count == null) ? 1 : count + 1);
+        }
+
+        // Sorts item entities
+        Map<String, Integer> treeMap = new TreeMap<String, Integer>(map);
+        List<ItemEntity> sortedItemEntityList = new ArrayList<ItemEntity>();
+        for (Map.Entry<String, Integer> entry : treeMap.entrySet()) {
+            sortedItemEntityList.add(itemDao.getItemByUuid(entry.getKey()));
+        }
+
+        // Reverse sort the collections
+        Collections.reverse(sortedItemEntityList);
+
+        return sortedItemEntityList;
     }
 }
